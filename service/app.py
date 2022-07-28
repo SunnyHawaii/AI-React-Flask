c279b21c114bc1f4fee8396b4b49faf0951ab820
SunnyHawaii <118558283+SunnyHawaii@users.noreply.github.com>
2022-07-28 08:22:55 +0000

    0.0.0

--- service/app.py
+++ service/app.py
@@ -6,4 +6,14 @@ import sys
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app,
-		  version = 
+		  version = "1.0",
+		  title = "Iris React-Flask App",
+		  description = "Predict results using a trained model")
+
+name_space = app.namespace('prediction', description='Prediction APIs')
+
+model = app.model('Prediction params',
+				  {'sepalLength': fields.Float(required = True,
+				  							   description="Sepal Length",
+    					  				 	   help="Sepal Length cannot be blank"),
+				  'sepalWidth': fields.Flo
