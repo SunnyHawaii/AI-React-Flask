9ac627ff523f7fb4806dd57bc07be0a8882ac633
SunnyHawaii <118558283+SunnyHawaii@users.noreply.github.com>
2022-10-21 13:37:19 +0000

    build: Adding MinusX

--- service/model_generator.py
+++ service/model_generator.py
@@ -3,4 +3,18 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-fr
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+from sklearn import datasets
+
+# Get the dataset
+dataset = datasets.load_iris()
+
+# Split the dataset into features and labels
+X = dataset.data
+y = dataset.target
+
+# Split the dataset into training (80%) and testing (20%) data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = True)
+
+# Build the
