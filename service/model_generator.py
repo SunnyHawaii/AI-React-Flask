2dc4ed564fd8859942b426a0f08727266f9aea2e
SunnyHawaii <118558283+SunnyHawaii@users.noreply.github.com>
2022-10-21 21:27:46 +0000

    project template

--- service/model_generator.py
+++ service/model_generator.py
@@ -17,4 +17,14 @@ y = dataset.target
 # Split the dataset into training (80%) and testing (20%) data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = True)
 
-# Build the
+# Build the classifier and make prediction
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+
+# Print the confusion matrix
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, prediction))
+
+# Save the model to disk
+joblib.dump(classifier, 'classifier.joblib')
